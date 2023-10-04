258c3e15654f421610db25d2bbfd232b7fa92afa
seunghwa <maga32@naver.com>
2023-10-05 07:03:55 +0900

    vite 프로젝트 전환10

--- frontend/src/pages/file/File.vue
+++ frontend/src/pages/file/File.vue
@@ -11,42 +11,47 @@
         <div class="col-12 col-md-4 row text-end">
           <div class="col-4 d-md-none"></div>
           <div class="col-6 col-md-8 px-2">
-            <input type="text" class="w-100 border border-secondary rounded-5 px-2" id="keyword" name="keyword" v-model="form.keyword">
+            <input type="text" class="w-100 border border-secondary rounded-5 px-2" id="keyword" v-model="form.keyword">
           </div>
           <div class="col-1 col-md-2 pointer" onClick="searchFileClick()"><i class="fa-solid fa-magnifying-glass"></i></div>
           <div class="col-1 col-md-2 ps-2">
-					<span class="pointer" data-bs-toggle="dropdown" aria-expanded="false">
-						<i class="fa-solid fa-wrench"></i>
-					</span>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><div class="dropdown-item pointer" onClick="toggleHiddenCheck()">
-                <span id="viewHiddenCheck" class="inactive"><i class="fa-solid fa-check"></i></span>
-                <i class="fa-solid fa-file-shield pe-2"></i>숨김파일보기
-              </div></li>
-              <li><div class="dropdown-item pointer" onClick="newFolder()" data-bs-toggle="modal" data-bs-target="#functionModal">
-                <i class="fa-solid fa-folder-plus pe-2"></i>새폴더
-              </div></li>
-              <li><div class="dropdown-item pointer" onClick="newFile()" data-bs-toggle="modal" data-bs-target="#functionModal">
-                <i class="fa-solid fa-file-circle-plus pe-2"></i>새파일
-              </div></li>
-            </ul>
+            <dropdown-menu withDropdownCloser direction="right">
+              <template #trigger>
+                <i class="pointer fa-solid fa-wrench"></i>
+              </template>
+
+              <template #body>
+                <ul class="dropdown-menu show" dropdown-closer>
+                  <li><div class="dropdown-item pointer" @click="toggleHiddenCheck">
+                    <span id="viewHiddenCheck" :class="form.viewHidden || 'inactive'"><i class="fa-solid fa-check"></i></span>
+                    <i class="fa-solid fa-file-shield pe-2"></i>숨김파일보기
+                  </div></li>
+                  <li><div class="dropdown-item pointer" onClick="newFolder()" data-bs-toggle="modal" data-bs-target="#functionModal">
+                    <i class="fa-solid fa-folder-plus pe-2"></i>새폴더
+                  </div></li>
+                  <li><div class="dropdown-item pointer" onClick="newFile()" data-bs-toggle="modal" data-bs-target="#functionModal">
+                    <i class="fa-solid fa-file-circle-plus pe-2"></i>새파일
+                  </div></li>
+                </ul>
+              </template>
+            </dropdown-menu>
           </div>
         </div>
 
         <table class="w-100">
           <tr>
-            <td class="text-center" style="width:35px;"><input id="checkAllFile" type="checkbox" class="form-check-input"></td>
+            <td class="text-center" style="width:35px;"><input type="checkbox" class="form-check-input" v-model="setting.checkAllFile"></td>
             <td style="width:80px;"></td>
             <td class="w-auto d-flex">
-              <span class="pointer" id="nameSort" @click="loadFileList('', '', 'name', '')">이름{{sortArrow('name')}}</span>
-              <span class="pointer px-3" id="sizeSort" @click="loadFileList('', '', 'size','')">크기{{sortArrow('size')}}</span>
+              <span class="pointer" @click="loadFileList('', '', 'name', '')">이름{{sortArrow('name')}}</span>
+              <span class="pointer px-3" @click="loadFileList('', '', 'size','')">크기{{sortArrow('size')}}</span>
             </td>
             <td class="text-end" style="width:15%">
-              <span class="pointer" id="typeSort" @click="loadFileList('', '', 'type','')">종류{{sortArrow('type')}}</span>
-              <span class="pointer d-none" id="pathSort" @click="loadFileList('', '', 'path','')">경로{{sortArrow('path')}}</span>
+              <span v-if="form.search" class="pointer" @click="loadFileList('', '', 'type','')">종류{{sortArrow('type')}}</span>
+              <span v-else class="pointer" @click="loadFileList('', '', 'path','')">경로{{sortArrow('path')}}</span>
             </td>
             <td class="text-center" style="width:20%">
-              <span class="pointer" id="dateSort" @click="loadFileList('', '', 'date','')">날짜{{sortArrow('date')}}</span>
+              <span class="pointer" @click="loadFileList('', '', 'date','')">날짜{{sortArrow('date')}}</span>
             </td>
           </tr>
         </table>
@@ -61,8 +66,11 @@
 
   <FileList
       :form="form"
-      :loadingList="loadingList"
+      :setting="setting"
+      :fileList="fileList"
   />
+
+  <button @click="loadFileList('', '', '','')">parent test</button>
 
   <!-- Modal -->
   <div class="modal fade" id="functionModal" tabindex="-1" aria-labelledby="functionModalLabel" aria-hidden="true">
@@ -85,7 +93,9 @@
 
 <script setup>
 import { onMounted, reactive, ref } from 'vue'
+import DropdownMenu from 'v-dropdown-menu'
 import { imageThumbnail, extensions } from '@/assets/extensions'
+
 import FileList from './FileList.vue'
 
 const form = reactive({
@@ -97,7 +107,15 @@ const form = reactive({
     viewHidden: false,
 })
 
-const loadingList = ref(false)
+const setting = reactive({
+  loadingList: false,
+  nowPath: '',
+  search: false,
+  checkAllFile: false,
+  dropdownSetting: false,
+})
+
+const fileList = reactive([])
 
 onMounted(()=> {
   $store.dispatch('link/addSiteHtml')
@@ -108,11 +126,32 @@ function sortArrow(sort) {
 }
 
 function loadFileList(shareLink, path, sort, order, keyword, resetKeyword = false, saveHistory = true) {
-  loadingList.value = true
-  form.sort = sort
-  form.order = (form.order == 'asc') ? 'desc' : 'asc'
-  loadingList.value = false
+  setting.loadingList = true
+  setting.checkAllFile = false
+  viewFileControlMenu()
+
+  form.shareLink = shareLink || form.shareLink
+  form.path = path || form.path
+  form.sort = sort || form.sort
+  form.order = order || form.order
+  form.keyword = resetKeyword ? '' : form.keyword
+
+  setting.search = !!form.keyword
+
+  setting.loadingList = false
+
+  console.log(form)
 }
+
+function viewFileControlMenu() {
+
+}
+
+function toggleHiddenCheck() {
+  form.viewHidden = !form.viewHidden
+  loadFileList('','','','')
+}
+
 </script>
 
 <style>
--- frontend/src/pages/file/FileList.vue
+++ frontend/src/pages/file/FileList.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="col-12" id="fileListContainer">
     <div id="fileList" class="px-2"></div>
-    <div id="loadingList" class="text-center m-2" :class="!loadingList || 'act'"><i class="fa-solid fa-circle-notch fa-spin" /> Loading... </div>
-    <button @click="testtest">test?</button>
-    {{loadingList}}
+    <div id="loadingList" class="text-center m-2" :class="!setting.loadingList || 'act'"><i class="fa-solid fa-circle-notch fa-spin" /> Loading... </div>
+    <button @click="childTest">child test</button>
   </div>
 </template>
 
 <script setup>
 import { defineProps, defineEmits } from 'vue'
 
-const props = defineProps(['form', 'loadingList'])
+const props = defineProps(['form', 'setting', 'fileList'])
 
-function testtest() {
-  console.log(props.form)
+function childTest() {
   console.log(props)
 }
 // const emit = defineEmits(["update"])
