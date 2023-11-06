6c2a3308a754a09d4157c046d2388f9fba85f9e3
seunghwa <maga32@naver.com>
2023-11-07 03:27:06 +0900

    html메타태그 @unhead/vue 사용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,17 +14,63 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { useHead } from '@unhead/vue'
 import HeaderMenu from './components/HeaderMenu.vue'
 import WingMenu from './components/WingMenu.vue'
+import { onMounted, ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HeaderMenu,
-    WingMenu,
-  }
-}
+const metaTitle       = ref('Every Cloud')
+const metaAuthor      = ref('EveryCloud')
+const metaDescription = ref('EveryCloud Share personal cloud for everyone.')
+const metaKeywords    = ref('EveryCloud, Share, Personal, Cloud')
+const metaUrl         = ref('')
+const metaImage       = ref('')
+
+useHead({
+  title: metaTitle,
+  meta: [
+      { name: 'title',                      content: metaTitle },
+      { name: 'author',                     content: metaAuthor },
+      { name: 'description',                content: metaDescription },
+      { name: 'keywords',                   content: metaKeywords },
+      { property: 'og:type',                content: 'website' },
+      { property: 'og:rich_attachment',     content: 'true' },
+      { property: 'og:site_name',           content: metaTitle },
+      { property: 'og:title',               content: metaTitle },
+      { property: 'og:description',         content: metaDescription },
+      { property: 'og:keywords',            content: metaKeywords },
+      { property: 'og:image',               content: metaImage },
+      { property: 'og:url',                 content: metaUrl },
+      { name: 'twitter:card',               content: 'summary_large_image' },
+      { name: 'twitter:site',               content: metaTitle },
+      { name: 'twitter:title',              content: metaTitle },
+      { name: 'twitter:description',        content: metaDescription },
+      { name: 'twitter:keywords',           content: metaKeywords },
+      { name: 'twitter:image',              content: metaImage },
+      { name: 'twitter:creator',            content: metaAuthor },
+      { itemProp: 'name',                   content: metaTitle },
+      { itemProp: 'description',            content: metaDescription },
+      { itemProp: 'keywords',               content: metaKeywords },
+      { itemProp: 'image',                  content: metaImage },
+      { name: 'apple-mobile-web-app-title', content: metaTitle },
+  ],
+})
+
+onMounted(() => {
+  $http.get('/settings/getMeta')
+    .then((response) => {
+      if(response.data) {
+        metaTitle.value       = response.data.metaTitle
+        metaAuthor.value      = response.data.metaAuthor
+        metaDescription.value = response.data.metaDescription
+        metaKeywords.value    = response.data.metaKeywords
+        metaUrl.value         = response.data.externalUrl
+        metaImage.value       = response.data.externalUrl + '/api/v1/file/getMetaImage'
+      }
+    })
+})
+
 </script>
 
 <style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import HttpModule from './modules/http'
-import { Form, Field, ErrorMessage } from 'vee-validate'
+import { createHead } from '@unhead/vue'
 import DropdownMenu from 'v-dropdown-menu'
+import { Form, Field, ErrorMessage } from 'vee-validate'
 import './modules/vee-validation'
 
 createApp(App)
   .use(HttpModule)                          // axios
   .use(router)                              // router
   .use(store)                               // store
+  .use(createHead())                        // meta
   .use(DropdownMenu)                        // dropdown
   .component("Form", Form)                  // validation form
   .component("Field", Field)                // validation field
